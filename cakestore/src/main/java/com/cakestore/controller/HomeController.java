1c0ce651066fa9d2583307e1f157449819360f10
ejiojeff <ejioforched@gmail.com>
2020-03-28 11:06:46 +0300

    Update HomeController.java

--- cakestore/src/main/java/com/cakestore/controller/HomeController.java
+++ cakestore/src/main/java/com/cakestore/controller/HomeController.java
@@ -57,8 +57,40 @@ public class HomeController {
 	}
 
 	@RequestMapping("/forgetPassword")
-	public String forgetPassword(Model model) {
+	public String forgetPassword(
+			HttpServletRequest request,
+			@ModelAttribute("email") String email,
+			Model model
+			) {
+
 		model.addAttribute("classActiveForgetPassword", true);
+
+		User user = userService.findByEmail(email);
+
+		if (user == null) {
+			model.addAttribute("emailNotExist", true);
+			return "myAccount";
+		}
+
+		String password = SecurityUtility.randomPassword();
+
+		String encryptedPassword = SecurityUtility.passwordEncoder().encode(password);
+		user.setPassword(encryptedPassword);
+
+		userService.save(user);
+
+		String token = UUID.randomUUID().toString();
+		userService.createPasswordResetTokenForUser(user, token);
+
+		String appUrl = "http://"+request.getServerName()+":"+request.getServerPort()+request.getContextPath();
+
+		SimpleMailMessage newEmail = mailConstructor.constructResetTokenEmail(appUrl, request.getLocale(), token, user, password);
+
+		mailSender.send(newEmail);
+
+		model.addAttribute("forgetPasswordEmailSent", "true");
+
+
 		return "myAccount";
 	}
 
@@ -80,7 +112,7 @@ public class HomeController {
 		}
 
 		if (userService.findByEmail(userEmail) != null) {
-			model.addAttribute("email", true);
+			model.addAttribute("emailExists", true);
 
 			return "myAccount";
 		}
@@ -89,7 +121,7 @@ public class HomeController {
 		user.setUsername(username);
 		user.setEmail(userEmail);
 
-		String password = SecurityUtility.randompassword();
+		String password = SecurityUtility.randomPassword();
 
 		String encryptedPassword = SecurityUtility.passwordEncoder().encode(password);
 		user.setPassword(encryptedPassword);
@@ -118,7 +150,6 @@ public class HomeController {
 
 	@RequestMapping("/newUser")
 	public String newUser(Locale locale, @RequestParam("token") String token, Model model) {
-
 		PasswordResetToken passToken = userService.getPasswordResetToken(token);
 
 		if (passToken == null) {
@@ -132,8 +163,7 @@ public class HomeController {
 
 		UserDetails userDetails = userSecurityService.loadUserByUsername(username);
 
-		Authentication authentication = new
-				UsernamePasswordAuthenticationToken(userDetails, userDetails.getPassword(),
+		Authentication authentication = new UsernamePasswordAuthenticationToken(userDetails, userDetails.getPassword(),
 				userDetails.getAuthorities());
 
 		SecurityContextHolder.getContext().setAuthentication(authentication);
