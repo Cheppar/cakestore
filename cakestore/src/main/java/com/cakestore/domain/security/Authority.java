b105f274691af7c0cfc846fa82c99940e84ed08b
ejiojeff <ejioforched@gmail.com>
2020-03-28 11:09:50 +0300

    Update Authority.java

--- cakestore/src/main/java/com/cakestore/domain/security/Authority.java
+++ cakestore/src/main/java/com/cakestore/domain/security/Authority.java
@@ -2,17 +2,16 @@ package com.cakestore.domain.security;
 
 import org.springframework.security.core.GrantedAuthority;
 
-public class Authority implements GrantedAuthority {
-
+public class Authority implements GrantedAuthority{
     private final String authority;
 
-
-    public Authority (String authority) {
+    public Authority(String authority) {
         this.authority = authority;
-
     }
+
     @Override
     public String getAuthority() {
         return authority;
     }
 }
+
