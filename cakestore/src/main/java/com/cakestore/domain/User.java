40df56b73d58655b978d44c840ebf2751469429f
ejiojeff <ejioforched@gmail.com>
2020-03-28 11:07:24 +0300

    Update User.java

--- cakestore/src/main/java/com/cakestore/domain/User.java
+++ cakestore/src/main/java/com/cakestore/domain/User.java
@@ -21,71 +21,65 @@ import com.cakestore.domain.security.UserRole;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
-public class User implements UserDetails {
+public class User implements UserDetails{
 
 	@Id
 	@GeneratedValue(strategy=GenerationType.AUTO)
-	@Column(name="id", nullable=false, updatable=false)
+	@Column(name="id", nullable = false, updatable = false)
 	private Long id;
 	private String username;
 	private String password;
 	private String firstName;
 	private String lastName;
 
-	@Column(name="email", nullable=false, updatable=false)
+	@Column(name="email", nullable = false, updatable = false)
 	private String email;
 	private String phone;
-	private boolean enabled = true;
+	private boolean enabled=true;
 
-	@OneToMany(mappedBy = "user", cascade=CascadeType.ALL, fetch=FetchType.EAGER)
+	@OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
 	@JsonIgnore
 	private Set<UserRole> userRoles = new HashSet<>();
-
 
 	public Long getId() {
 		return id;
 	}
-
 	public void setId(Long id) {
 		this.id = id;
 	}
-
 	public String getUsername() {
 		return username;
 	}
-
 	public void setUsername(String username) {
 		this.username = username;
 	}
-
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
 	public String getFirstName() {
 		return firstName;
 	}
-
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
-
 	public String getLastName() {
 		return lastName;
 	}
-
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
-
 	public String getEmail() {
 		return email;
 	}
-
 	public void setEmail(String email) {
 		this.email = email;
 	}
-
 	public String getPhone() {
 		return phone;
 	}
-
 	public void setPhone(String phone) {
 		this.phone = phone;
 	}
@@ -93,57 +87,39 @@ public class User implements UserDetails {
 	public void setEnabled(boolean enabled) {
 		this.enabled = enabled;
 	}
-
 	public Set<UserRole> getUserRoles() {
 		return userRoles;
 	}
-
 	public void setUserRoles(Set<UserRole> userRoles) {
 		this.userRoles = userRoles;
 	}
-
 	@Override
 	public Collection<? extends GrantedAuthority> getAuthorities() {
-		Set<GrantedAuthority> authorities = new HashSet<>();
-		userRoles.forEach(ur -> authorities.add(new
-		Authority(ur.getRole().getName())));
-		return authorities;
+		Set<GrantedAuthority> authorites = new HashSet<>();
+		userRoles.forEach(ur -> authorites.add(new Authority(ur.getRole().getName())));
+
+		return authorites;
 	}
-
-
 	@Override
 	public boolean isAccountNonExpired() {
 		// TODO Auto-generated method stub
 		return true;
 	}
-
 	@Override
 	public boolean isAccountNonLocked() {
 		// TODO Auto-generated method stub
 		return true;
 	}
-
 	@Override
 	public boolean isCredentialsNonExpired() {
 		// TODO Auto-generated method stub
 		return true;
 	}
-	@Override
-		public boolean isEnabled() {
-			return enabled;
-		}
 
 	@Override
-	public String getPassword() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public void setPassword(String encryptedPassword) {
-		// TODO Auto-generated method stub
-
+	public boolean isEnabled() {
+		return enabled;
 	}
 
 
-	}
-
+}
